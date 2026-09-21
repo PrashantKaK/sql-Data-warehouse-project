9cbd2d90dcef06e21e42d5fa806180425ac497c6
Prashant <65370723+PrashantKaK@users.noreply.github.com>
2026-09-22 00:30:00 +0530

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,16 @@
+-- Create DataWarehouse database
+
+USE master;
+
+CREATE DATABASE DataWarehouse;
+
+
+USE DataWarehouse;
+
+-- Create Schemas
+
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
